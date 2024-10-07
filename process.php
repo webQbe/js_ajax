62af3bb35203a5edaf1c9ec3afa280915a1bf0f6
webQbe <webcubesolutionslk@gmail.com>
2024-10-07 11:31:52 +0530

    Compared HTTP POST & AJAX GET forms with AJAX POST form

--- process.php
+++ process.php
@@ -2,10 +2,16 @@
 
 echo 'Processing... ';
 
+
+// Check for POST variable from ajax4.html
+if(isset($_POST['name'])){
+    // add variable to string
+    echo 'POST: Your name is '. $_POST['name'];
+}
+
+
 // Check for GET variable from ajax4.html
 if(isset($_GET['name'])){
-    
     // add variable to string
     echo 'GET: Your name is '. $_GET['name'];
-
 }
