c5b1adb3a8cee22d889f1430d02f36762972f18a
webQbe <webcubesolutionslk@gmail.com>
2024-10-07 10:37:23 +0530

    Passed value to process.php file and fetched processed data

--- process.php
+++ process.php
@@ -1,3 +1,11 @@
 <?php
 
 echo 'Processing... ';
+
+// Check for GET variable from ajax4.html
+if(isset($_GET['name'])){
+    
+    // add variable to string
+    echo 'GET: Your name is '. $_GET['name'];
+
+}
